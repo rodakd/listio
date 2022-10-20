1a23901bb3080b72477b4a3f4b930d44ba669c18
Dawid Rodak <dawidrodak@yahoo.com>
2022-10-20 02:07:36 +0200

    Better room id

--- pages/api/create-room.js
+++ pages/api/create-room.js
@@ -2,6 +2,7 @@ import cache from 'memory-cache'
 import shortid from 'shortid'
 
 export default function handler(req, res) {
+    shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@')
     const roomId = shortid.generate()
     cache.put(roomId, { id: roomId, lists: [] })
     res.status(200).json({ roomId })
